e322d0cfcd96cf35d0d5272bc81bca9d84f34553
meghana398 <meghanazoar@gmail.com>
2026-06-09 16:48:13 +0530

    Update imports in density_occupancy module

--- app/kpis/density_occupancy/__init__.py
+++ app/kpis/density_occupancy/__init__.py
@@ -1,3 +1,3 @@
 # app/kpis/your_kpi/__init__.py
-from .detector import people_density_measurement  # noqa: F401
-from .detector import occupancy   
+from .detector import DensityOccupancyKPI  # noqa: F401
+#from .detector import occupancy   
